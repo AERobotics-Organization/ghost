f06674e87b9092581fc4877728de378bbbc40048
Truman Purnell <truman.purnell@gmail.com>
2018-12-21 17:29:14 -0700

    completes tree parsing for white space delimited radley code

--- src/main/radley/tree.mjs
+++ src/main/radley/tree.mjs
@@ -1,21 +1,24 @@
-import { BLANK_LINE } from '../../resources'
-
 import RadleyStatement from './statement'
 
 export default class RadleyTree {
-    static init(code, block = new Array(), depth = 0) {
+    static init(code, block = new Array(), depth = 0, index = [0]) {
         let line, statement
-        while ((line = code.shift()) !== undefined)
-            if (!(statement = new RadleyStatement(line)).isBlank())
-
-                if (statement.depth > depth)
-                    block.push(RadleyTree.init(code, [statement], statement.depth))
+        while ((line = code[index[0]]) !== undefined)
+            if ((statement = new RadleyStatement(line)).isBlank())
+                index[0]++
+            else
+                if (statement.depth > depth) {
+                    index[0]++
+                    block.push(RadleyTree.init(code, [statement], statement.depth, index))
+                }
 
                 else if (statement.depth < depth)
                     return block
 
-                else
+                else {
+                    index[0]++
                     block.push(statement)
+                }
 
         return block
     }
