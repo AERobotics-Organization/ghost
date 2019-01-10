765b5f2c1dff94bec5769ebc7780430cd769415b
Truman Purnell <truman.purnell@gmail.com>
2019-01-09 22:12:22 -0800

    tests passing for matrix mult

--- src/test/index.mjs
+++ src/test/index.mjs
@@ -1,5 +1,9 @@
 import radley from '../main'
 
+const A = { header: { shape: [4, 4], strides: [4, 1], data: [4, 5, 2, 4, 5, 4, 6, 7, 4, 5, 3, 5, 6, 7, 3, 5] } }
+const B = { header: { shape: [4, 4], strides: [4, 1], data: [4, 5, 2, 4, 5, 4, 6, 7, 4, 5, 3, 5, 6, 7, 3, 5] } }
+const R = { header: { shape: [4, 4], strides: [4, 1], data: new Float64Array(16) } }
+
 radley.suite(function ({ A, B, R }) {
     const source = []
 
@@ -7,12 +11,14 @@ radley.suite(function ({ A, B, R }) {
         for (let j = 0; j < R.header.shape[1]; j++) {
             const mults = []
             for (let k = 0; k < A.header.shape[1]; k++) {
-                mults.push(` A.header.data[${i * A.shape[1] + k}] * B.header.data[${k * B.shape[1] + j}] `)
+                mults.push(` A.header.data[${i * A.header.shape[1] + k}] * B.header.data[${k * B.header.shape[1] + j}] `)
             }
             source.push(`R.header.data[${source.length}]=${mults.join(' + ')}`)
         }
     }
 
     source.push('return R')
-    return new Function('A', 'B', 'R', source.join('\n'))
-})
+    return new Function('{ A, B, R }', source.join('\n'))
+}).call({ A, B, R })
+
+console.log(R.header.data)
