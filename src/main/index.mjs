f72c055cb737fb675c120ccf3a3933f2c1498d38
Truman Purnell <truman.purnell@gmail.com>
2019-02-19 14:03:55 -0800

    restructures radley to allow for unbounded tier count

--- src/main/index.mjs
+++ src/main/index.mjs
@@ -1,16 +1,13 @@
 import { makeCaller, makeRouter, wrap } from './utils.mjs'
 
 export default class RadleySuite {
-    constructor({ router, tractable, methods: { generic, optimized } }) {
+    constructor({ ...tiers, hash }) {
         this.suite = {}
-        this.tractable = tractable
+        this.hash = hash
+        this.tiers = tiers
 
-        const routerSource = router.toString()
-        this.call = makeCaller(routerSource).bind(this)
-        this.route = makeRouter(routerSource).bind(this)
-
-        this.generic = wrap(generic).bind(this)
-        this.optimized = wrap(optimized).bind(this)
+        this.call = makeCaller(this.tiers)
+        this.route = makeRouter(this.hash)
     }
 
     static suite(opts) {
--- src/main/utils.mjs
+++ src/main/utils.mjs
@@ -1,52 +1,36 @@
 
-export const wrap = function (func) {
-    if (!new RegExp('new\\s+Function').test(func.toString()))
-        return function () { return func }
-
-    return func
-}
-
-export const makeRouter = function (routerSource) {
-    const source = []
-
-    makeTree(routerSource).reduce(function (path, block, i) {
-        const route = routerSource.substring(block[0], block[block.length - 1] + 1)
-
-        if (i === 0)
-            source.push(`const i${i} = this.suite${route} || (this.suite${route} = {})`)
-        else
-            source.push(`const i${i} = i${i - 1}${route} || (i${i - 1}${route} = {})`)
-
-        return path + route
-    }, '')
-
-    return new Function('args', `${source.join('\n')}; return i${source.length - 1}`)
-}
-
-export const makeCaller = function (routerSource) {
-    const [routerPath] = routerSource.match(/this.suite\[(\s|\S)+]/g)
-
+export const makeRouter = function (hash) {
     return new Function('args', `
-        let func = this.route(args)
-
-        if (!(func.constructor === Function)) {
-            if (this.tractable(args))
-                func = ${routerPath.split(/\s/g).filter(function (e) { return !!e }).join(' ')} = this.optimized(args)
+        ${hash.map(function (field, i) {
+            if (!i)
+                return `const i${i} = this.suite[${field}] || (this.suite[${field}] = {})`
             else
-                func = ${routerPath.split(/\s/g).filter(function (e) { return !!e }).join(' ')} = this.generic(args)
+                return `const i${i} = i${i - 1}[${field}] || (i${i - 1}[${field}] = {})`
+        }).join('\n')}
+
+        return i${hash.length - 1}
+    `)
+}
+
+export const makeCaller = function (tiers) {
+    return new Function('args', `
+        let func, result = this.route(args)
+
+        if (result.constructor === Object) {
+            ${makeTierChecks(tiers)}   
         }
 
         return func(args)
     `)
 }
 
-function makeTree(source, target = ['[', ']'], p = [-1], ctx = []) {
-    for (; ++p[0] < source.length;)
-        if (source[p[0]] === target[0]) // open brace
-            ctx.push(makeTree(source, target, p, [p[0]]))
-
-        else if (source[p[0]] === target[1]) // close brace
-            return ctx.concat(p[0])
-
-    return ctx
+export const makeTierChecks = function (tiers) {
+    return Object.values(tiers)
+        .map(function (tier, i) {
+            return (!i ? '' : 'else') +
+                `if(${tier.criteria}){
+                    func = result[args.method] = tiers.methods[method] || tiers.methods
+                }`
+        })
+        .join('\n')
 }
